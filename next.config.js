a17868f6356016ab057e0e9a4d93437398e3de4d
Matias Duran <matiduraan@gmail.com>
2024-01-03 20:35:29 -0300

    Add env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
   images: {
     domains: ["media.lmneuquen.com"],
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_ARG_DATA: process.env.NEXT_PUBLIC_API_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 module.exports = nextConfig;
